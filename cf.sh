e43e01b56eb07936ffbc9e25310cb6272e067910
wangrui06 <wangrui06@shengpay.com>
2023-05-14 00:43:03 +0800

    modify cf.sh

--- cf.sh
+++ cf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 VERSION=${git describe --tags --abbrev=0}
-sed s/'REPLACE_VERSION_HERE'/'"${VERSION}"'/g .env.production
+sed s/'REPLACE_VERSION_HERE'/'"$VERSION"'/g .env.production
 yarn build-only
