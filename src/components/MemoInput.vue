d37d518cdd0888f8e8d69f9372578787393f49e6
kingwrcy <yoyo@openmbox.net>
2023-06-01 22:32:44 +0800

    优化webp格式图片

--- src/components/MemoInput.vue
+++ src/components/MemoInput.vue
@@ -115,7 +115,8 @@
               height="100"
               lazy
               class="rd hover:shadow-2xl"
-              :src="img.url + (img.suffix || '')"
+              :src="img.url + (img.fileType.includes('webp') ? '' : img.suffix || '')"
+              :fallback-src="img.url"
               :preview-src="img.url"
               :intersection-observer-options="{
                 root: '#image-scroll-container',
